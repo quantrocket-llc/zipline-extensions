75a3baf22bd7911cdba5686eae6c7e119c9880b1
Brian <brian@quantrocket.com>
2018-02-09 11:13:21 -0500

    change custom error inheritance

--- zipline_extensions/errors.py
+++ zipline_extensions/errors.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from zipline.errors import ZiplineError
+class ZiplineExtensionError(Exception):
+    pass
 
-class BadIngestionArgument(ZiplineError):
+class BadIngestionArgument(ZiplineExtensionError):
     pass
